a354ce55d020383ef84a0cd6f355bb5907b7c11a
XimanWu <99100905+xiiiim@users.noreply.github.com>
2022-09-08 17:59:16 -0500

    Update and rename 844. Backspace String Compare to 844. Backspace String Compare.py

--- 844. Backspace String Compare.py
+++ 844. Backspace String Compare.py
@@ -1,4 +1,30 @@
-Solution 1: Stack
+# Easy
+'''
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+Example 2:
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+Example 3:
+
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+
+'''
+
+# Solution 1: Stack
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         def build(l):
@@ -12,7 +38,7 @@ class Solution:
 
         return build(s) == build(t)
         
-Solution 2:
+# Solution 2:
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         s1, t1 = len(s)-1, len(t)-1
